4f094d027e842b0462ae4425079e8a1de8fde555
Rfkinrikhwan <rifkinurikhwan9@gmail.com>
2024-12-11 10:11:10 +0700

    Finish create bottom navigation and add section skills

--- app/components/Layout/index.tsx
+++ app/components/Layout/index.tsx
@@ -7,27 +7,27 @@ export default function LayoutCustom({ children }: { children: ReactNode }) {
     const location = useLocation();
 
     return (
-        <motion.div className="min-h-screen sm:px-96 relative">
+        <div className="min-h-screen sm:px-96 relative">
             <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808018_1px,transparent_1px),linear-gradient(to_bottom,#80808018_1px,transparent_1px)] bg-[size:30px_30px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_0%,#000_80%,transparent_100%)]"></div>
 
-            <AnimatePresence mode="wait">
-                <motion.div
-                    key={location.pathname}
-                    initial={{ opacity: 0, y: 20 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{
-                        duration: 0.3,
-                        type: "spring",
-                        stiffness: 120,
-                        damping: 15
-                    }}
-                    className="w-full"
-                >
-                    {children}
-                </motion.div>
-            </AnimatePresence>
+            {/* <AnimatePresence mode="wait"> */}
+            {/* <motion.div
+                key={location.pathname}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                    duration: 0.3,
+                    type: "spring",
+                    stiffness: 120,
+                    damping: 15
+                }}
+                className="w-full"
+            > */}
+            {children}
+            {/* </motion.div> */}
+            {/* </AnimatePresence> */}
 
             <NavigationContainer />
-        </motion.div>
+        </div>
     );
 }
--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,11 +1,37 @@
 import { MetaFunction } from "@remix-run/node";
+import { CodeXml } from "lucide-react";
+import Marquee from "~/components/ui/marquee";
 
 export const meta: MetaFunction = () => {
     return [
         { title: "About | Rifki" },
-        { name: "description", content: "Welcome to Remix!" },
+        { name: "description", content: "Allo Guys, I'm Rifki" },
     ];
 };
+
+const skills = [
+    { "name": "JavaScript", "icon": "javascript.png" },
+    { "name": "TypeScript", "icon": "typescript.png" },
+    { "name": "PHP", "icon": "php.png" },
+    { "name": "Laravel", "icon": "laravel.png" },
+    { "name": "Dart", "icon": "dart.png" },
+    { "name": "Flutter", "icon": "flutter.png" },
+    { "name": "React JS", "icon": "react.png" },
+    { "name": "React Native", "icon": "reactnative.png" },
+    { "name": "Vue JS", "icon": "vue.png" },
+    { "name": "Remix JS", "icon": "remix.png" },
+    { "name": "Next JS", "icon": "nextjs.png" },
+    { "name": "Tailwind", "icon": "tailwind.png" },
+    { "name": "Bootstrap", "icon": "bootstrap.png" },
+]
+
+const tools = [
+    { "name": "Github", "icon": "github.webp" },
+    { "name": "Postman", "icon": "postman.webp" },
+    { "name": "Herd", "icon": "herd.webp" },
+    { "name": "Vite", "icon": "vite.webp" },
+    { "name": "Expo", "icon": "expo.webp" },
+]
 
 export default function AboutPage() {
     return (
@@ -52,6 +78,41 @@ export default function AboutPage() {
                 <div className="border rounded-xl h-52"></div>
                 <div className="border rounded-xl h-52"></div>
             </div>
+
+            <div className="border my-4" />
+
+            <div className="flex items-center gap-3">
+                <CodeXml />
+                <h1 className="text-xl font-bold">Skills and Tools</h1>
+            </div>
+
+            <p className="mt-4 leading-7">
+                I master several programming languages and some of their framewoks and the following tools that I often use
+            </p>
+
+            <Marquee pauseOnHover className="[--duration:50s] mt-3">
+                {skills.map((data: any) => {
+                    return (
+                        <div className="py-2 px-5 rounded-full shadow flex items-center gap-2">
+                            <img src={data.icon} alt="" className="h-7 w-7" />
+                            {data.name}
+                        </div>
+                    )
+                })}
+            </Marquee>
+
+            <Marquee pauseOnHover reverse className="[--duration:15s] mt-3">
+                {tools.map((data: any) => {
+                    return (
+                        <div className="py-2 px-5 rounded-full shadow flex items-center gap-2">
+                            {data.name}
+                            <img src={data.icon} alt="" className="h-5 w-5" />
+                        </div>
+                    )
+                })}
+            </Marquee>
+
+            <div className="mb-48" />
         </div>
     );
 }
